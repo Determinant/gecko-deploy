d06cb1a89eefaa810596cbb2322d52cc35f61e41
Determinant <tederminant@gmail.com>
2020-03-10 02:04:53 -0400

    ...

--- run.sh
+++ run.sh
@@ -5,10 +5,11 @@ node_file=nodes.yml
 
 function die { echo "$1"; exit 1; }
 function print_help {
-echo "Usage: $0 [--group] [--help] COMMAND WORKDIR
+echo "Usage: $0 [--group] [--nodes] [--help] COMMAND WORKDIR
 
     --help                      show this help and exit
-    --group                     specify the ansible group"
+    --group                     specify the ansible group name
+    --nodes                     specify the ansible inventory"
     exit 0
 }
 
@@ -17,38 +18,72 @@ function check_argnum {
     [[ "$1" -eq "$argnum" ]] || die "incorrect argnum: got $argnum, $1 expected"
 }
 
-function _start {
-    workdir="$1"
+function _check_id {
+    id=${1%/}
+    [[ "$id" =~ ^[0-9a-zA-Z_-|]+$ ]]
+}
+
+function check_id {
+    id=${1%/}
+    (_check_id "$id" && [[ -f "$id/nodes.yml" ]]) || die "invalid id"
+    workdir="$id"
+}
+
+function try_lock {
+    set -o noclobber && echo > "$1/lock" || die "another run.sh is busy"
+}
+
+function unlock {
+    rm "$1/lock"
+}
+
+function _new {
+    _check_id "$1"
+    workdir="$id"
     if [[ -a "$workdir" ]]; then
         die "$workdir already exists"
     fi
     mkdir -p "$workdir"
     cp "$node_file" "$workdir/nodes.yml"
     cp -r group_vars/ "$workdir"
+    try_lock "$workdir"
     ansible-playbook -i "$workdir/nodes.yml" ansible/start.yml --extra-vars "run_id=$workdir host_group=$host_group"
+    unlock "$workdir"
+}
+
+function _start {
+    check_id "$1"
+    try_lock "$workdir"
+    ansible-playbook -i "$workdir/nodes.yml" ansible/start.yml --extra-vars "run_id=$workdir host_group=$host_group"
+    unlock "$workdir"
 }
 
 function _stop {
-    workdir=${1%/}
-    [[ -f "$workdir/nodes.yml" ]] || die "invalid id"
-    ansible-playbook -i "$1/nodes.yml" ansible/stop.yml --extra-vars "run_id=$workdir host_group=$host_group"
+    check_id "$1"
+    try_lock "$workdir"
+    ansible-playbook -i "$workdir/nodes.yml" ansible/stop.yml --extra-vars "run_id=$workdir host_group=$host_group"
+    unlock "$workdir"
 }
 
 function _check {
-    workdir=${1%/}
-    [[ -f "$workdir/nodes.yml" ]] || die "invalid id"
-    ansible-playbook -i "$1/nodes.yml" ansible/check.yml --extra-vars "run_id=$workdir host_group=$host_group"
+    check_id "$1"
+    try_lock "$workdir"
+    ansible-playbook -i "$workdir/nodes.yml" ansible/check.yml --extra-vars "run_id=$workdir host_group=$host_group"
+    unlock "$workdir"
 }
 
 function _fetch {
-    workdir=${1%/}
-    [[ -f "$workdir/nodes.yml" ]] || die "invalid id"
-    ansible-playbook -i "$1/nodes.yml" ansible/fetch.yml --extra-vars "run_id=$workdir host_group=$host_group"
+    check_id "$1"
+    try_lock "$workdir"
+    ansible-playbook -i "$workdir/nodes.yml" ansible/fetch.yml --extra-vars "run_id=$workdir host_group=$host_group"
+    unlock "$workdir"
 }
 
 function _reset {
-    workdir=${1%/}
-    ansible-playbook -i "$1/nodes.yml" ansible/reset.yml --extra-vars "run_id=$workdir host_group=$host_group"
+    check_id "$1"
+    try_lock "$workdir"
+    ansible-playbook -i "$workdir/nodes.yml" ansible/reset.yml --extra-vars "run_id=$workdir host_group=$host_group"
+    unlock "$workdir"
 }
 
 getopt --test > /dev/null
@@ -76,6 +111,7 @@ done
 cmd="$1"
 shift 1
 case "$cmd" in
+    new) check_argnum 1 "$@" && _new "$1" ;;
     start) check_argnum 1 "$@" && _start "$1" ;;
     stop) check_argnum 1 "$@" && _stop "$1" ;;
 #    status) check_argnum 1 "$@" && status_all "$1" ;;
