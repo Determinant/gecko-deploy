2bdc0f4b8b6a32f67968fb323d71f1c6e81cbcaf
Determinant <tederminant@gmail.com>
2020-03-09 21:54:39 -0400

    ...

--- filter_plugins/custom_plugins.py
+++ filter_plugins/custom_plugins.py
@@ -7,9 +7,11 @@ class FilterModule(object):
         return {
             'path_join': self.path_join,
             'not': self._not,
+            'all': self._all,
         }
 
     def path_join(self, paths):
         return os.path.join(*paths)
 
     def _not(self, x): return not x
+    def _all(self, l): return all(l)
