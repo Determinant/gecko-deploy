f711af4931a188f69d6c03a2d00928fe926a86b7
Determinant <tederminant@gmail.com>
2020-03-11 16:04:43 -0400

    ...

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
+basedir="$(dirname $(realpath "${BASH_SOURCE[0]}"))"
 export node_group=nodes
 export node_setup_group=nodes_setup
-export node_file=nodes.ini
-export group_vars=./group_vars
-basedir="$(dirname $(realpath "${BASH_SOURCE[0]}"))"
+export node_file="$basedir/nodes.ini"
+export group_vars="$basedir/group_vars"
 export build_tasks="$basedir/app/build.yml"
 export reset_tasks="$basedir/app/reset.yml"
 export run_tasks="$basedir/app/run.yml"
